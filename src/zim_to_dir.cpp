94221d55a2d8924c8d5ac3b16f3f0714c983c9f5
Nicolai <cybernic@users.noreply.github.com>
2022-04-14 17:29:41 +0200

    Update zim_to_dir.cpp

--- src/zim_to_dir.cpp
+++ src/zim_to_dir.cpp
@@ -34,7 +34,7 @@ namespace fs = std::filesystem;
 
 /** Holds disambiguation patterns in titles for languages we support. */
 std::map<std::string, std::string> disambig = {
-    {"hu", "\\(egyértelműsítő lap\\)$"}, {"en", "\\(disambiguation\\)$"}
+    {"hu", "\\(egyértelműsítő lap\\)$"}, {"en", "\\(disambiguation\\)$"}, {"fr", "\\(homonymie\\)$"}, {"pt", "\\(desambiguação\\)$"}, {"it", "\\(disambigua\\)$"}, {"de", "\\(Begriffsklärung\\)$"}
 };
 
 /** A join function similar to what Python has. */
